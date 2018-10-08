6f4fa87af7bacf98814bf822781f88b9636b1b09
Ruzsa Balazs <cellux@omkamra.hu>
2018-10-08 15:52:48 +0200

    simplify communication with Rasid
    we don't use luamode_loadstring(): it breaks if the region is too long
    
    we just send blocks directly to Rasid without any pre-processing
    except addition of the final -- line

--- rasid-mode.el
+++ rasid-mode.el
@@ -9,42 +9,55 @@
     (define-key map (kbd "<f4>") 'rasid-send-block)
     (define-key map (kbd "C-c c") 'rasid-send-block)
     (define-key map (kbd "C-c C-c") 'rasid-send-block)
-    (define-key map (kbd "<f5>") 'lua-restart-with-whole-file)
-    (define-key map (kbd "<f6>") 'lua-send-region)
+    (define-key map (kbd "<f5>") 'rasid-restart)
+    (define-key map (kbd "<f6>") 'rasid-send-region)
     map))
 
 (define-derived-mode rasid-mode lua-mode "Rasid"
-  "Major mode for editing Rasid scripts"
-  (set (make-local-variable 'lua-default-application) "rasid"))
+  "Major mode for editing Rasid scripts")
 
-(defun rasid-break-lines (lua-string)
-  (with-temp-buffer
-    (insert lua-string)
-    (goto-char (point-min))
-    (while (search-forward "\\n" nil t)
-      (insert "'..\n'"))
-    (buffer-string)))
+(defun rasid-start-process ()
+  (interactive)
+  (setq lua-process-buffer (make-comint "rasid" "rasid"))
+  (setq lua-process (get-buffer-process lua-process-buffer))
+  (set-process-query-on-exit-flag lua-process nil)
+  (if (called-interactively-p 'any)
+      (switch-to-buffer lua-process-buffer)))
 
-(defun rasid-send-string (lua-send-string str)
-  "Send STR plus an empty comment line (--) to Rasid."
-  (funcall lua-send-string
-           (concat (rasid-break-lines str) "\n--\n")))
+(defun rasid-get-create-process ()
+  "Return active Lua process creating one if necessary."
+  (or (and (comint-check-proc lua-process-buffer)
+           lua-process)
+      (rasid-start-process))
+  lua-process)
 
-(advice-add 'lua-send-string :around #'rasid-send-string)
+(defun rasid-send-string (str)
+  (process-send-string (rasid-get-create-process)
+                       (concat str "\n--\n")))
+
+(defun rasid-send-region (start end)
+  (interactive "r")
+  (rasid-send-string (buffer-substring-no-properties start end))
+  (when lua-always-show (lua-show-process-buffer)))
+
+(defun rasid-send-buffer ()
+  "Send whole buffer to lua subprocess."
+  (interactive)
+  (rasid-send-region (point-min) (point-max)))
 
 (defun rasid-send-line ()
   "Send the current line to Rasid."
   (interactive)
   (when (not (process-live-p lua-process))
-    (lua-send-buffer))
-  (lua-send-region (line-beginning-position) (line-end-position)))
+    (rasid-send-buffer))
+  (rasid-send-region (line-beginning-position) (line-end-position)))
 
 (defun rasid-send-block (pos)
   "Send the current block (region delineated by empty lines) to Rasid."
   (interactive "d")
   ; if Rasid is not running, start it and send the whole buffer
   (when (not (process-live-p lua-process))
-    (lua-send-buffer))
+    (rasid-send-buffer))
   (let ((current-line-is-empty (save-excursion
                                  (beginning-of-line)
                                  (looking-at "^[ \t]*$"))))
@@ -63,21 +76,27 @@
                        (forward-line))
                      (point))))
           (if (and (>= pos start) (< pos end))
-              (lua-send-region start end)
+              (rasid-send-region start end)
             (error "Not inside a block")))))))
 
 (defun rasid-send-block-and-play-next-item (pos)
   (interactive "d")
   (rasid-send-block pos)
-  (lua-send-string "R.play()\n"))
+  (rasid-send-string "R.play()"))
 
 (defun rasid-send-block-and-play-next-item-forever (pos)
   (interactive "d")
   (rasid-send-block pos)
-  (lua-send-string "R.play(nil, true)\n"))
+  (rasid-send-string "R.play(nil, true)"))
 
 (defun rasid-stop ()
   (interactive)
-  (lua-send-string "R.stop()\n"))
+  (rasid-send-string "R.stop()"))
+
+(defun rasid-restart ()
+  "Restart lua subprocess and send whole file as input."
+  (interactive)
+  (lua-kill-process)
+  (rasid-send-buffer))
 
 (provide 'rasid-mode)
